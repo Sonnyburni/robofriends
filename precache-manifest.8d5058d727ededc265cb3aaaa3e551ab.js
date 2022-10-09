b5bd49cb8ba1196ba3423d123eb00a60490f4f36
Sonnyburni <sonnyburniston@yahoo.co.uk>
2022-10-09 11:00:16 +0100

    Updates

---
+++ precache-manifest.8d5058d727ededc265cb3aaaa3e551ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2f6e666419bd76c7dbe",
+    "url": "/robofriends/static/css/main.ce4c14bf.chunk.css"
+  },
+  {
+    "revision": "d2f6e666419bd76c7dbe",
+    "url": "/robofriends/static/js/main.d2f6e666.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "7b0a8fd637932a1855d4",
+    "url": "/robofriends/static/css/2.1c2f2415.chunk.css"
+  },
+  {
+    "revision": "7b0a8fd637932a1855d4",
+    "url": "/robofriends/static/js/2.7b0a8fd6.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "421337b82af32c46b2a06fbddc92c6cb",
+    "url": "/robofriends/index.html"
+  }
+];
